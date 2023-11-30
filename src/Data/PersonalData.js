dc5515d43d4479158b0bf3dc6a2183a77280cfce
khlifnihel <khlifnihel@fsm.u-monastir.tn>
2023-11-30 12:58:23 +0100

    CV update

--- src/Data/PersonalData.js
+++ src/Data/PersonalData.js
@@ -6,7 +6,7 @@ const PersonalData = {
     email: "khlifnihel@fsm.u-monastir.tn",
     address: "El Omrane , Monastir , Tunisia",
     aboutMe: "Hi Everyone, I Nihel Khlif from Sidi Bouzid, Tunisia.I learned C, C++, Python, JavaScript etc, Worked with libraries & frameworks like ReactJS, Django. Have a keen knowledge of databases like MySQL, MongoDB and Oracle. ",
-    resumeLink: 'https://drive.google.com/file/d/1Pf5TxDUvCoDFH2mOhfJR0nEnvn6Gd8Fg/view?usp=sharing'
+    resumeLink: 'https://drive.google.com/drive/u/0/folders/10Oo2859rdwB0DH6qrkEs4SWFGXNYKIla'
 }
 export const autoTypeData = ['3nd Year IT Grad', 'Developer'];
 export default PersonalData;
