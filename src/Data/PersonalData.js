20ac3fc37c835a9a342cb265b4a6d33357d9e80c
khlifnihel <khlifnihel@fsm.u-monastir.tn>
2023-11-30 13:06:59 +0100

    Data  update

--- src/Data/PersonalData.js
+++ src/Data/PersonalData.js
@@ -8,5 +8,5 @@ const PersonalData = {
     aboutMe: "Hi Everyone, I Nihel Khlif from Sidi Bouzid, Tunisia.I learned C, C++, Python, JavaScript etc, Worked with libraries & frameworks like ReactJS, Django. Have a keen knowledge of databases like MySQL, MongoDB and Oracle. ",
     resumeLink: 'https://drive.google.com/drive/u/0/folders/10Oo2859rdwB0DH6qrkEs4SWFGXNYKIla'
 }
-export const autoTypeData = ['3nd Year IT Grad', 'Developer'];
+export const autoTypeData = ['3rd Year IT Grad', 'Developer'];
 export default PersonalData;
